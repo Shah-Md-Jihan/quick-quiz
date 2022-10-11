3dfdb6edbeb76f09cdd7fb72444b178dee59930b
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-10-11 22:30:11 +0600

    responsive line chart added in statistic component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Blogs from './components/Blogs/Blogs';
 import Home from './components/Home/Home';
 import Statistics from './components/Statistics/Statistics';
 import TopicDetail from './components/TopicDetail/TopicDetail';
-import Topics from './components/Topics/Topics';
 import Main from './layouts/Main';
 
 function App() {
@@ -33,6 +32,7 @@ function App() {
         },
         {
           path: 'statistics',
+          loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
           element: <Statistics></Statistics>
         },
         {
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData();
+    console.log(data.data);
     return (
         <div>
-            <h2>This is statistics component</h2>
+            <Container>
+                <h2 className='text-start py-5'>Total Quiz Statistics</h2>
+                <ResponsiveContainer width="95%" height={400}>
+                    <LineChart className='text-danger' width={600} height={400} data={data.data}>
+                        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                    </LineChart>
+                </ResponsiveContainer>
+
+            </Container>
+
         </div>
     );
 };
