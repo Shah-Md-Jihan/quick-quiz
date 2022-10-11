09baa3a25993e3462740a7af7bf1acc6d79544af
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-10-11 11:11:09 +0600

    responsive nav bar, some component and react router funtionality added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink, Link } from 'react-router-dom';
+import './Header.css';
+
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand>
+                        <Link className='text-decoration-none text-white' to='/'>QuickQuiz</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link>
+                                <NavLink className='text-decoration-none me-4 text-white' to="/topics">Topics</NavLink>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <NavLink className='text-decoration-none me-4 text-white' to="/statistics">Statistics</NavLink>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <NavLink className={`text-decoration-none me-4 text-white ${({ isActive }) => {
+                                    return isActive ? "active" : undefined;
+                                }}
+`} to="/blogs">Blog</NavLink>
+                            </Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
